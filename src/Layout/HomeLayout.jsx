32cb8b2ee94f563214ba71cbd77d608fe46296e2
Nissan Barua <baruanissan55.com>
2024-11-29 12:28:49 +0600

    leftside bar done

--- src/Layout/HomeLayout.jsx
+++ src/Layout/HomeLayout.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import Header from "../Components/Header";
 import LatestNews from "../Components/LatestNews";
+import Navbar from "../Components/Navbar";
+import LeftSideBar from "../Components/LayoutComponents/LeftSideBar";
 
 const HomeLayout = () => {
   return (
     <div className="container mx-auto">
       <header>
         <Header></Header>
-        <LatestNews></LatestNews>
+        <section>
+          <LatestNews></LatestNews>
+        </section>
+        <section className="items-center">
+          <Navbar></Navbar>
+        </section>
       </header>
-      <main></main>
+      <main className="mt-10 grid md:grid-cols-12 gap-3">
+        <aside className="col-span-3">
+          <LeftSideBar></LeftSideBar>{" "}
+        </aside>
+        <section className="col-span-6">Main</section>
+        <aside className="col-span-3">right side</aside>
+      </main>
     </div>
   );
 };
